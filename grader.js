841cff266acf923c06dcdcd2141f1793c98d43a4
plassen <github@pedroplassen.org>
2013-07-11 23:53:55 +0000

    grader stuff

--- grader.js
+++ grader.js
@@ -22,6 +22,8 @@ References:
 */
 
 var fs = require('fs');
+var sys = require('util');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
@@ -65,10 +67,29 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    var file = HTMLFILE_DEFAULT;
+    if (program.url) {
+      rest.get(program.url).on('complete', function(result) {
+        if (result instanceof Error) {
+          sys.puts('Error: ' + result.message);
+    }
+    else {
+       fs.writeFileSync(file, result);
+       var checkJson = checkHtmlFile(file, program.checks);
+       var outJson = JSON.stringify(checkJson, null, 4);
+       console.log(outJson);  
+    }
+});
+    }
+    else if (program.file) {
+      file = program.file;
+      var checkJson = checkHtmlFile(file, program.checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);  
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
